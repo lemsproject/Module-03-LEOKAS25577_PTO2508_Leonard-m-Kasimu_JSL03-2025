3695cd78f734132d821b0b1b9113eb40114b737a
lemsproject <leonardmsd@gmail.com>
2026-02-28 23:34:56 +0200

    modifying the Array of tasks inside the sript

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const initialTasks = [
+  {
+    id: 1,
+    title: "gym",
+    description: "work those muscles",
+    status: "todo",
+  },
+  {
+    id: 2,
+    title: "Study",
+    description: "Work those brain muscles",
+    status: "doing",
+  },
+  {
+    id: 3,
+    title: "Sleep",
+    description: "Rest that body",
+    status: "done",
+  },
+];
