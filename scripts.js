625a741d8bb3ead1cc64a7ac42d612a1b6ef5296
lemsproject <leonardmsd@gmail.com>
2026-02-28 23:52:57 +0200

    adding 3 more arrays inside the script

--- scripts.js
+++ scripts.js
@@ -17,4 +17,24 @@ const initialTasks = [
     description: "Rest that body",
     status: "done",
   },
+  {
+    id: 4,
+    title: "correct JSL02",
+    description: "Make corrections to JSL02 before attempting JSL03",
+    status: "done",
+  },
+  {
+    id: 5,
+    title: "Work on JSL03",
+    description:
+      "Read the project brief and user stories to understand the requirements for JSL03, implement a solution",
+    status: "todo",
+  },
+  {
+    id: 6,
+    title: "Revise JavaScript concepts",
+    description:
+      "Practice JS concepts to make sure I understand the fundamentals",
+    status: "doing",
+  },
 ];
